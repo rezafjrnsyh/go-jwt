92a5d0ff076257dc2c8662d6a49b2322a052cb2e
Reza <rezafajriansyah21.com>
2022-01-03 11:02:35 +0700

    [FEATURE] Auth token middleware

--- main.go
+++ main.go
@@ -4,14 +4,38 @@ import (
 	"github.com/gin-gonic/gin"
 )
 
+type authheader struct {
+	AuthorizationHeader string `header:"Authorization"`
+}
+
+type Credential struct {
+	Username string 	`json:"username"`
+	Password string		`json:"password"`
+}
+
 func main() {
 	r := gin.Default()
+	r.Use(AuthtokenMiddleware())
 
-	type authheader struct {
-		AuthorizationHeader string `header:"Authorization"`
-	}
+	r.POST("/login", func(c *gin.Context) {
+		var user Credential
+		if err := c.BindJSON(&user); err != nil {
+			c.JSON(400, gin.H{
+				"message": "can't bind struct",
+			})
+			return
+		}
 
-	r.GET("/customer", func (c *gin.Context)  {
+		if user.Username == "enigma" && user.Password == "123" {
+			c.JSON(200, gin.H{
+				"token": "123",
+			})
+		} else {
+			c.AbortWithStatus(401)
+		}
+	})
+
+	r.GET("/customer", func(c *gin.Context) {
 		h := authheader{}
 
 		if err := c.ShouldBindHeader(&h); err != nil {
@@ -28,11 +52,11 @@ func main() {
 			return
 		}
 		c.JSON(401, gin.H{
-			"message" : "Unauthorized",
+			"message": "Unauthorized",
 		})
 	})
 
-	r.GET("/product", func (c *gin.Context)  {
+	r.GET("/product", func(c *gin.Context) {
 		h := authheader{}
 
 		if err := c.ShouldBindHeader(&h); err != nil {
@@ -49,12 +73,38 @@ func main() {
 			return
 		}
 		c.JSON(401, gin.H{
-			"message" : "Unauthorized",
+			"message": "Unauthorized",
 		})
 	})
 
 	err := r.Run("localhost:8888")
 	if err != nil {
 		panic(err)
+	}
+}
+
+func AuthtokenMiddleware() gin.HandlerFunc {
+	return func(c *gin.Context) {
+		if c.Request.URL.Path == "/login" {
+			c.Next()
+		} else {
+			h := authheader{}
+
+			if err := c.ShouldBindHeader(&h); err != nil {
+				c.JSON(401, gin.H{
+					"message": "Unathorized",
+				})
+				c.Abort()
+			}
+
+			if h.AuthorizationHeader == "123" {
+				c.Next()
+			} else {
+				c.JSON(401, gin.H{
+					"message": "Unauthorized",
+				})
+				c.Abort()
+			}
+		}
 	}
 }
